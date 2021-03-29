4426d9e87f96f075f06a8c5a2d5b5f1b9ded5bd3
Byunghoon <Byunghoon@DESKTOP-O5HUBCG>
2021-03-29 21:16:57 +0900

    Web Compiler

--- MentoMenti/src/main/java/Mento/Menti/Project/controller/HomeController.java
+++ MentoMenti/src/main/java/Mento/Menti/Project/controller/HomeController.java
@@ -18,4 +18,9 @@ public class HomeController {
     	return "CompileTestView";
     }
     
+    @RequestMapping(value="/Compile")
+    public String index3() {
+    	return "Compile";
+    }
+    
 }
