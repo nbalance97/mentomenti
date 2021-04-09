39ba3928e27bfad8a64b49a449e8b1368eb94500
byunghoon <byunghoon@DESKTOP-4LN6VP2>
2021-04-09 15:07:44 +0900

    재수정

--- MentoMenti/src/main/java/Mento/Menti/Project/MentoMentiApplication.java
+++ MentoMenti/src/main/java/Mento/Menti/Project/MentoMentiApplication.java
@@ -1,7 +1,14 @@
 package Mento.Menti.Project;
 
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 @SpringBootApplication
 public class MentoMentiApplication {
@@ -9,5 +16,16 @@ public class MentoMentiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MentoMentiApplication.class, args);
 	}
-
+	/*
+	@Bean
+	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+		SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+		sessionFactory.setDataSource(dataSource);
+		
+		Resource[] res = new PathMatchingResourcePatternResolver().getResources("classpath:mappers/*Mapper.xml");
+		sessionFactory.setMapperLocations(res);
+		
+		return sessionFactory.getObject();
+	}
+	*/
 }
