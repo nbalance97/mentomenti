0809aa19eef6af76ff164db903fd5cb43fb30874
Byunghoon <nbalance__@kyonggi.ac.kr>
2021-05-06 14:22:50 +0900

    [Web Compiler Docker] C 테스트

--- MentoMenti/src/main/java/Mento/Menti/Project/WebCompiler/WebCompiler.java
+++ MentoMenti/src/main/java/Mento/Menti/Project/WebCompiler/WebCompiler.java
@@ -2,6 +2,7 @@ package Mento.Menti.Project.WebCompiler;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
@@ -9,8 +10,10 @@ import java.util.ArrayList;
 
 public class WebCompiler {
 	public static WebCompiler instance = null;
-	public String doc = "/home/mentomenti_compiler";
+	public String shared_document = "/docshare/";
 	// local에서 컴파일할때는 c:/Temp로 수정하면 됨.
+	
+	public boolean[] docker_status = {false, false, false};
 	
 	public static WebCompiler getInstance() {
 		if (instance == null)
@@ -27,22 +30,53 @@ public class WebCompiler {
 		return java_default;
 	}
 	
+	public String getDockerName(int idx) {
+		if (idx == 0)
+			return "mmenti1";
+		if (idx == 1)
+			return "mmenti2";
+		if (idx == 2)
+			return "mmenti3";
+		return null;
+	}
+	
 	public String compileC(String SRC, String input) {
 		/* C언어 컴파일 실행 함수 */
 		try {
-			setFile("C", SRC);
-			/* 컴파일 파트 */
-		 	ArrayList<String> command = setCommand("C");
-		 	executeCMD(command, null);
-		 	
-		 	/* 실행 파트 */
-		 	command.clear();
-		 	command.add("./MentoMenti.exe"); // 리눅스 경로로 수정
-		 	String temp = executeCMD(command, input);
-		 	File f = new File(doc + "/MentoMenti.exe"); // 파일 삭제
-		 	f.delete();
-		 	
-		 	return temp;
+			for (int i = 0; i<docker_status.length; i++) { // 현재 이용가능한 도커 검색
+				if (docker_status[i] == false) {
+					docker_status[i] = true;
+					String docker_name = getDockerName(i);
+					setFile(docker_name, "C", SRC);
+					
+					/* 컴파일 파트 */
+				 	ArrayList<String> dockercmd = setCommand(docker_name, "C");
+				 	executeCMD(dockercmd, null);
+				 	
+				 	/* 가져오는 파트 */
+				 	File result = new File(shared_document + "/" + docker_name + "/compileResult.txt"); 
+				 	File err = new File(shared_document + "/" + docker_name + "/compileErr.txt");
+				 	
+				 	FileReader fr = new FileReader(result);
+				 	FileReader errfr = new FileReader(err);
+				 	StringBuffer temp = new StringBuffer("");
+				 	StringBuffer errtemp = new StringBuffer("");
+				 	int ch = 0;
+				 	while ((ch = fr.read()) != -1)
+				 		temp.append(ch);
+				 	while ((ch = errfr.read()) != -1)
+				 		errtemp.append(ch);
+				 	
+				 	docker_status[i] = false;
+				 	
+				 	if (errtemp.length() > 0) // 에러 존재시 에러 리턴
+				 		return errtemp.toString();
+				 	else
+				 		return temp.toString();
+				}
+			}
+			System.out.println("이용 가능한 도커가 없습니다.");
+			return null; // 
 		}
 		catch (Exception e) {
 			e.printStackTrace();
@@ -53,10 +87,11 @@ public class WebCompiler {
 	public String compilePython(String SRC, String input) { 
 		/* 파이썬 컴파일 실행 함수 */
 		try {
+			return null;
 			/* 파이썬 같은 경우는 인터프리터라서 컴파일->실행 필요 X */
-			setFile("python", SRC);
-		 	ArrayList<String> command = setCommand("python");
-		 	return executeCMD(command, input);
+			//setFile("python", SRC);
+		 	//ArrayList<String> command = setCommand("python");
+		 	//return executeCMD(command, input);
 		}
 		catch (Exception e) {
 			e.printStackTrace();
@@ -66,48 +101,48 @@ public class WebCompiler {
 	
 	public String compileJava(String SRC, String input) {
 		try {
-			setFile("java", SRC);
-			ArrayList<String> command = setCommand("java");
-			executeCMD(command, null);
+			return null;
+			//setFile("java", SRC);
+			//ArrayList<String> command = setCommand("java");
+			//executeCMD(command, null);
 			
-			command.clear();
-			command.add("java");
-			command.add("MentoMenti");
-			return executeCMD(command, input);
+			//command.clear();
+			//command.add("java");
+			//command.add("MentoMenti");
+			//return executeCMD(command, input);
 		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
 		}
 	}
 
-	public ArrayList<String> setCommand(String mode) { 
+	public ArrayList<String> setCommand(String docker, String mode) { 
 		ArrayList<String> cmd = new ArrayList<String>();
 		if (mode.contentEquals("python")){
-		 	cmd.add("python"); 
-		 	cmd.add(doc + "/MentoMenti.py");
+		 	cmd.add("sh"); 
+		 	cmd.add(shared_document + "/" + docker + "/compilePython.sh");
 		} else if (mode.contentEquals("C")) {
-			cmd.add("gcc");
-			cmd.add("-o");
-			cmd.add(doc + "/MentoMenti.exe");
-			cmd.add(doc + "/MentoMenti.c"); 
+			cmd.add("sh");
+			cmd.add(shared_document + "/" + docker + "/compileC.sh");
 		} else if (mode.contentEquals("java")) {
-			cmd.add("javac");
-			cmd.add("MentoMenti.java");
+			cmd.add("sh");
+			cmd.add(shared_document + "/" + docker + "/compileJava.sh");
 		}
 		return cmd;
 	}
 	
-	public void setFile(String mode, String SRC) throws Exception {
+	public void setFile(String docker, String mode, String SRC) throws Exception {
 		/* 컴파일러에 맞추어서 파일 저장 */
 		FileWriter fw = null;
+
 		if (mode.contentEquals("python")) {
-		 	fw = new FileWriter(doc + "/MentoMenti.py");
+		 	fw = new FileWriter(shared_document + "/" + docker + "/MentoMenti.py");
 		 	fw.write(SRC);
 		} else if (mode.contentEquals("C")) {
-		 	fw = new FileWriter(doc + "/MentoMenti.c");
+		 	fw = new FileWriter(shared_document + "/" + docker + "/MentoMenti.c");
 		 	fw.write(SRC);
 		} else if (mode.contentEquals("java")) {
-		 	fw = new FileWriter(doc + "/MentoMenti.java");
+		 	fw = new FileWriter(shared_document + "/" + docker + "/MentoMenti.java");
 		 	fw.write(SRC);
 		}
 		
@@ -119,9 +154,8 @@ public class WebCompiler {
 	
 	
 	public String executeCMD(ArrayList<String> cmd, String input) throws Exception {
-		System.out.println(cmd);
 		ProcessBuilder processbuilder = new ProcessBuilder(cmd);
-		processbuilder = processbuilder.directory(new File(doc)); // 현재 실행폴더를 c:/Temp로 전환
+		//processbuilder = processbuilder.directory(new File(doc)); // 현재 실행폴더를 c:/Temp로 전환
 		Process process = processbuilder.start();
 	 	
 	 	/* 입력 값 받고 적용하기 위한 writer 설정 */
