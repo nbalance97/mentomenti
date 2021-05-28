b9430f6bf551cbc0a93fc3ce5e308678c8f9c5d9
Byunghoon <nbalance__@kyonggi.ac.kr>
2021-05-28 15:35:22 +0900

    [annotation]

--- MentoMenti/src/main/java/Mento/Menti/Project/controller/WebConfig.java
+++ MentoMenti/src/main/java/Mento/Menti/Project/controller/WebConfig.java
@@ -1,9 +1,12 @@
 package Mento.Menti.Project.controller;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
+	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/upload/**").addResourceLocations("file:////root/user/");
 	}
